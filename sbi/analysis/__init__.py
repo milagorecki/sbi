ba32dc8c7e05b20461ea65633f4d83d2f3126de1
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-06-27 17:08:01 +0200

    add plotting functions from v0.18.0

--- sbi/analysis/__init__.py
+++ sbi/analysis/__init__.py
@@ -2,5 +2,10 @@ from sbi.analysis.conditional_density import (
     conditional_corrcoeff,
     eval_conditional_density,
 )
-from sbi.analysis.plot import conditional_pairplot, pairplot
+from sbi.analysis.plot import (
+    conditional_pairplot,
+    pairplot,
+    conditional_marginal_plot,
+    marginal_plot,
+)
 from sbi.analysis.sensitivity_analysis import ActiveSubspace
